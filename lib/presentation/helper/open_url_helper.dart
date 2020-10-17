195c7446782561ec09fdbb819aec6a9fc4602c49
Alexey Lopukh <lopukh911@gmail.com>
2020-10-17 21:43:08 +0300

    added ability join room by id

--- lib/presentation/helper/open_url_helper.dart
+++ lib/presentation/helper/open_url_helper.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:messager/interactor/join_room_interactor.dart';
+import 'package:messager/presentation/components/popups.dart';
 import 'package:messager/presentation/di/user_scope_data.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -9,20 +10,21 @@ class OpenUrlHelper {
   openUrl(String url, BuildContext context) async {
     IncomingUriHelperData data = IncomingUriHelper().incomingUriHandler(Uri.parse(url));
     if (data != null)
-      _handleUrl(data, context);
+      Popups.showProgressPopup(context, _handleUrl(data, context));
     else if (await canLaunch(url)) {
       await launch(url);
     }
   }
 
-  _handleUrl(IncomingUriHelperData data, BuildContext context) {
+  Future _handleUrl(IncomingUriHelperData data, BuildContext context) {
     switch (data.type) {
       case IncomingUriType.JoinRoom:
         IncomingUriJoinRoomData uriJoinRoomData = data;
         if (uriJoinRoomData.uuid != null && uriJoinRoomData.roomId != null) {
-          JoinRoomInteractor(UserScopeWidget.of(context)).join(uriJoinRoomData.roomId);
+          return JoinRoomInteractor(UserScopeWidget.of(context)).join(uriJoinRoomData.roomId);
         }
         break;
     }
+    return null;
   }
 }
